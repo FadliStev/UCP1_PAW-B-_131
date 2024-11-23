9a5f21edf2bff7bc0861e99d03f62bee65e8195e
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-11-23 14:20:08 +0700

    menambah get index

--- app.js
+++ app.js
@@ -1,4 +1,13 @@
 const express = require('express');
 const app = express();
 const todoRoutes = require('./routes/todo.js');
+const { json } = require('body-parser');
 const port = 3000;
+
+app.use(express/json());
+
+app.use('/todo', todoRoutes);
+app.set('view engine', 'ejs');
+app.get('/', (req, res) => {
+    res.render('index');
+});
