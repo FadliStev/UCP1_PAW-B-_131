95d2b589e635e0fd3d70d832424f2ec320a77ff5
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-11-23 13:29:51 +0700

    menambah data pada array

--- routes/BibitTanaman.js
+++ routes/BibitTanaman.js
@@ -1,3 +1,18 @@
 import express, { Router } from "express";
 
 const router = express.Router();
+
+const bibit = [
+
+    {
+        nama_tanaman: "Padi",
+        jenis: "Briyani",
+        kota_suplier: "Jakarta",
+        tgl_masuk: 18,
+        bulan_masuk: 3,
+        tahun_masuk: 20024
+        
+
+    }
+
+]
