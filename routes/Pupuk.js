6be73fc775d093e4529385dc6a57a5e24f879071
Wiranto Akbar Fadlillah <alvinex1999@gmail.com>
2024-11-23 13:39:13 +0700

    menambah data pada mock data pupuk

--- routes/Pupuk.js
+++ routes/Pupuk.js
@@ -1,3 +1,14 @@
 import express, { Router } from "express";
 
 const router = express.Router();
+
+const pupuk = [
+
+    {
+        nama:"Pupes",
+        tahun_produksi:"2001",
+        tahun_kadaluarsa:"2003",
+        jenis:"Pupuk Kering"
+    }
+
+]
